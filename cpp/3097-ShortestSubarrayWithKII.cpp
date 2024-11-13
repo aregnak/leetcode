db5a16d5d39f53649534721b760fa8e0dbc22114
Areg Nakashian <ss4crifice@gmail.com>
2024-11-13 15:19:26 -0500

    3097 solved C++

---
+++ cpp/3097-ShortestSubarrayWithKII.cpp
@@ -0,0 +1,55 @@
+/*
+Solution by aregnak, commented code isn't needed on leetcode's code section.
+*/
+
+// #include <iostream>
+// #include <vector>
+// #include <climits>
+
+// using namespace std
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) 
+    {
+        int n = nums.size();
+        int minLength = INT_MAX;
+        int left = 0;
+        int currentOr = 0;
+        vector<int> bits(32, 0);
+        
+        for (int right = 0; right < n; right++) 
+        { 
+            currentOr |= nums[right];
+            
+            for (int bit = 0; bit < 32; bit++) 
+            {
+                if (nums[right] & (1 << bit)) 
+                {
+                    bits[bit]++;
+                }
+            }
+
+            while (left <= right && currentOr >= k) 
+            {
+                minLength = min(minLength, right - left + 1);
+                int newOr = 0;
+
+                for (int bit = 0; bit < 32; bit++) 
+                {
+                    if (nums[left] & (1 << bit)) 
+                    {
+                       bits[bit]--;
+                    }
+                    if (bits[bit] > 0)
+                    {
+                       newOr |= (1 << bit);
+                    }
+                }
+                currentOr = newOr;
+                left++;
+            }
+        }
+
+        return (minLength == INT_MAX) ? -1 : minLength;
+    }
+};
